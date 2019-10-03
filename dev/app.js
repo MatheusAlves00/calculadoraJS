ebcc69429f72d81ac8e77562cda899a67afcccfa
MatheusAlves00 <39467858+MatheusAlves00@users.noreply.github.com>
2019-10-02 21:34:25 -0300

    calculadora

---
+++ dev/app.js
@@ -0,0 +1,6 @@
+var botao = document.querySelector('num');
+
+document.querySelector(".num").addEventListener("click", function(){
+    var display = document.querySelector("#display");
+    debugger
+});
