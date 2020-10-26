8faf012e425ac3a76a7438fb4294e3afcdc21887
Jaicke Santos <jaicke-santos@outlook.com>
2020-10-26 19:50:28 -0300

    feat(dark_mode): Adds buttom to toggle dark mode

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var botoes = document.querySelectorAll('input');
+let botoes = document.querySelectorAll('input');
+let darkButton = document.querySelector('.dark-btn')
 
 botoes.forEach(function(botao){
   botao.addEventListener("click", function(){
     var display = document.querySelector("#display");
-    
+
     if (this.value == '='){
       display.value = eval(display.value);
     } else if(this.value == 'C') {
@@ -15,3 +16,7 @@ botoes.forEach(function(botao){
   });
 });
 
+darkButton.addEventListener('click', (e) => {
+  document.documentElement.classList.toggle('dark-mode')
+  e.target.classList.toggle('dark-mode')
+})
