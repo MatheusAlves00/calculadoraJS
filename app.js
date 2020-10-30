8f38335bc76ed21ca6e9970e280a6e4520a9e8d5
Jaicke Santos <jaicke-santos@outlook.com>
2020-10-30 20:40:55 -0300

    feat(calculadora): Adds accouts history

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 let botoes = document.querySelectorAll('input');
 let darkButton = document.querySelector('.dark-btn')
+let historyPanel = document.querySelector('.history')
 
 botoes.forEach(function(botao){
   botao.addEventListener("click", function(){
     var display = document.querySelector("#display");
 
     if (this.value == '='){
+      historyPanel.appendChild(createHistoryItem(display.value))
       display.value = eval(display.value);
+      historyPanel.appendChild(createHistoryItem(display.value, true))
+      historyPanel.scrollTop = historyPanel.scrollHeight
     } else if(this.value == 'C') {
       display.value = '';
     } else {
@@ -20,3 +24,16 @@ darkButton.addEventListener('click', (e) => {
   document.documentElement.classList.toggle('dark-mode')
   e.target.classList.toggle('dark-mode')
 })
+
+
+function createHistoryItem(text, result = false){
+  let node = document.createElement("p");
+  if(result){
+    textnode = document.createTextNode(`= ${text}`);
+  }else{
+    textnode = document.createTextNode(text);
+  }
+  node.appendChild(textnode);
+
+  return node
+}
